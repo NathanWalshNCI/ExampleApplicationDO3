82325b5b5007c8f471caa4f5529fdf5ea80d4173
NathanWalshNCI <x22154086@student.ncirl.ie>
2023-03-09 17:30:54 +0000

    updated pipeline to be secured with https

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,7 @@ pm2 stop example_app
 cd ExampleApplicationDO3/
 #Install application dependencies
 npm install
+echo $PRIVATE_KEY > privatekey.pem
+echo $SERVER > server.crt
 #Start the application with the process name example_app using PM2
 pm2 start ./bin/www --name example_app

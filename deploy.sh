16be5b171f7d506e64fe316393c7b714790e9ba2
NathanWalshNCI <x22154086@student.ncirl.ie>
2023-04-02 20:47:37 +0100

    added variable name in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,25 +11,25 @@ fi
 # Pull down the instance from DockerHub
 docker pull $IMAGE_NAME
 
-# Check if a docker container exists with the name of the node_app. If it does remove the container
-CONTAINER_EXISTS=$(docker ps -a | grep node_app) 
+# Check if a docker container exists with the name of the $CONTAINER_NAME. If it does remove the container
+CONTAINER_EXISTS=$(docker ps -a | grep $CONTAINER_NAME) 
 if [ "$CONTAINER_EXISTS" ]
 then
-	docker rm node_app
+	docker rm $CONTAINER_NAME
 fi
 
-# Create a container called node_app that is available on port 8443 from our docker image
-docker create -p 8443:8443 --name node_app $IMAGE_NAME
+# Create a container called $CONTAINER_NAME that is available on port 8443 from our docker image
+docker create -p 8443:8443 --name $CONTAINER_NAME $IMAGE_NAME
 # Write the private key to a file
 echo $PRIVATE_KEY > privatekey.pem
 # Write the server key to a file
 echo $SERVER > server.crt
-# Add the private key to the node_app docker container
-docker cp ./privatekey.pem node_app:/privatekey.pem
-# Add the server key to the node_app docker container
-docker cp ./server.crt node_app:/server.crt
-# Start the node_app container
-docker start node_app
+# Add the private key to the $CONTAINER_NAME docker container
+docker cp ./privatekey.pem $CONTAINER_NAME:/privatekey.pem
+# Add the server key to the $CONTAINER_NAME docker container
+docker cp ./server.crt $CONTAINER_NAME:/server.crt
+# Start the $CONTAINER_NAME container
+docker start $CONTAINER_NAME
 
 
 #sudo apt update && sudo apt install nodejs npm
